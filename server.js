7b30e0754ac5a81f671ea05d68c989206d9c8bfe
dvicjon <devinvjones@gmail.com>
2021-02-23 17:47:39 -0500

    12.2.5

--- server.js
+++ server.js
@@ -22,13 +22,23 @@ const db = new sqlite3.Database('./db/election.db', err => {
 //     console.log(rows);
 // });
 
-// GET a single candidate
-// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//     if(err) {
-//       console.log(err);
-//     }
-//     console.log(row);
-// });
+// Get single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates 
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+});
 
 // Delete a candidate
 // ? = placeholder, a prepared statement - filled in with real values at runtime 
@@ -40,15 +50,32 @@ const db = new sqlite3.Database('./db/election.db', err => {
 // });
 
 // Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-              VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-// ES5 function, not arrow function, to use this
-db.run(sql, params, function(err, result) {
-  if (err) {
-    console.log(err);
-  }
-  console.log(result, this.lastID);
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// // ES5 function, not arrow function, to use this
+// db.run(sql, params, function(err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this.lastID);
+// });
+
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
 });
 
 // Default response for any other request(Not Found) Catch all
